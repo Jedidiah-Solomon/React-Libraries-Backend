539164ddcd67c4d6eeb88190a62c13820bb1d75c
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-09-02 14:48:38 +0100

    Update user_biodataController.js

--- controllers/user_biodataController.js
+++ controllers/user_biodataController.js
@@ -156,7 +156,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 1 * 60 * 1000,
     });
 
